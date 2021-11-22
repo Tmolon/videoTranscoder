83b1c37d018235f8535ebbd23f5e99b33bcacc5b
Tolga Molon <tolgamolon@gmail.com>
2021-11-22 22:19:48 +0300

    Update settings.py

--- video_transcoder/video_transcoder/settings.py
+++ video_transcoder/video_transcoder/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2(d&=v97!%p6d0imj6znujcx=cp0kwt40w+8w1l(n9wc($@mjt'
+SECRET_KEY = 'your secret key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
